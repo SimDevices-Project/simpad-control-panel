eee605e8f326467c7bca32c138e968c89a80ea08
Handle <mail@bysb.net>
2018-11-19 22:05:18 +0800

    修改 中国台湾 翻译标志

--- app/js/lang/langList.js
+++ app/js/lang/langList.js
@@ -1,7 +1,7 @@
 var langList = [
   { description: 'zh-CN', title: '中文简体（中国大陆）' },
   { description: 'zh-SG', title: '中文简体（新加坡）' },
-  { description: 'zh-TW', title: '中文繁體（臺灣）' },
+  { description: 'zh-TW', title: '中文正體（臺灣）' },
   { description: 'en-US', title: 'English' },
   { description: 'ja-JP', title: '日本語' },
   { description: 'de-DE', title: 'Deutsch (Deutschland)' },
