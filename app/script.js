08c94f172d969fbf72ceeb2c8beafeedb59e0c91
Handle <mail@bysb.net>
2018-12-03 05:15:53 +0800

    版本号更新

--- app/script.js
+++ app/script.js
@@ -6,7 +6,7 @@ var app = require('electron').app
 const path = require('path')
 var AdmZip = require('adm-zip')
 
-const APP_VERSION = 'v0.144'
+const APP_VERSION = 'v0.145'
 
 var clearAllTimeout = () => {
   for (var i = setTimeout(() => {}, 0); i; i--) {
