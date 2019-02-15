a29eb24ca9d2e36519c0f2a41e59f53418682c2b
Handle <mail@bysb.net>
2019-02-15 23:52:25 +0800

    更新 0.149.0 提供Lite

---
+++ app/devices/simpadv2lite/device.js
@@ -0,0 +1,7 @@
+module.exports = {
+  vendorId: 0x8088,
+  productId: 0x0003,
+  description: 'simpadv2lite',
+  endpoint: '01',
+  maxEndpoint: 3
+}
---
+++ app/devices/simpadv2lite/name.lang.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'zh-CN': 'SimPad 版本2 轻量版',
+  'zh-SG': 'SimPad 版本2 轻量版',
+  'en-US': 'SimPad v2 Lite'
+}
