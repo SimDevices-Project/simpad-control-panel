800f60d7d98d6f235e2e755dce6d52641ca5ec7a
Handle <mail@bysb.net>
2020-07-30 17:10:25 +0800

    版本号转为 0.165

--- app/script.js
+++ app/script.js
@@ -6,7 +6,7 @@ var app = require('electron').app
 const path = require('path')
 var AdmZip = require('adm-zip')
 
-const APP_VERSION = 'v0.163'
+const APP_VERSION = 'v0.165'
 
 var clearAllTimeout = () => {
   for (var i = setTimeout(() => {}, 0); i; i--) {
